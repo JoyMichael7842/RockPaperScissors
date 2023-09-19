4b823972004b694750c20c73d43fc0caac2b8040
Joy <joynalanda@gmail.com>
2023-09-19 13:52:44 -0400

    added game logic

--- code.js
+++ code.js
@@ -8,6 +8,40 @@ function getComputerChoice(){
     return moves[randomNumber]
 }
 
+function playRound(playerSelection,computerSelection){
+
+    playerSelection = playerSelection.toLowerCase()
+    if (playerSelection == computerSelection){
+        return 'Its a Draw!'
+    }
+    if(playerSelection=='rock'){
+        if(computerSelection == 'scissors'){
+            return 'you Win! Rock beats scissors'
+        }
+        else{
+            return 'you Lose! paper beats rock'
+        }
+    }
+    if(playerSelection=='paper'){
+        if(computerSelection == 'rock'){
+            return 'you Win! paper beats rock'
+        }
+        else{
+            return 'you Lose! scissors beats rock'
+        }
+    }
+    if(playerSelection=='scissors'){
+        if(computerSelection == 'paper'){
+            return 'you Win! scissors beats paper'
+        }
+        else{
+            return 'you Lose! rock beats scissors'
+        }
+    }
+}
 const moves = ['rock', 'paper', 'scissors']
 
-console.log(getComputerChoice())
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
