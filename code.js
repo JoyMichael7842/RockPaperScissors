e8f230e79a9ae8f5d84dfcb752bc074339677ca8
Joy <joynalanda@gmail.com>
2023-09-23 21:12:20 -0400

    added buttons and score

--- code.js
+++ code.js
@@ -1,3 +1,58 @@
+
+
+const playerScoretext = document.querySelector('#player_score') 
+const computerScoretext = document.querySelector('#computer_score') 
+const resultText = document.querySelector('#result') 
+const buttons= document.querySelectorAll('.button')
+console.log(buttons)
+buttons.forEach(button=>button.addEventListener('click',getPlayerChoice))
+let playerscore = 0
+let computerscore = 0
+let result=''
+function getPlayerChoice(e){
+    console.log(e.target.textContent)
+    result = playRound(e.target.textContent,getComputerChoice())
+    console.log(result)
+    updateScores(result)
+    
+}
+
+const moves = ["rock", 'paper', 'scissors']
+
+function updateScores(result){
+    if(result=='Win'){
+        playerscore +=1
+        console.log(playerscore)
+        if(playerscore==5){
+            resultText.textContent = 'You got to 5, You Win!'
+            playerscore = 0
+            computerscore = 0
+            playerScoretext.textContent = 'Player Score: '+playerscore.toString()
+            computerScoretext.textContent = 'Computer Score: '+computerscore.toString()
+            return
+        }
+        playerScoretext.textContent = 'Player Score: '+playerscore.toString()
+        resultText.textContent = 'Result: You Win'
+    }
+    if(result=='Lose'){
+        computerscore +=1
+        console.log(computerscore)
+        if(computerscore==5){
+            resultText.textContent = 'Computer got to 5, You Lose!'
+            playerscore = 0
+            computerscore = 0
+            playerScoretext.textContent = 'Player Score: '+playerscore.toString()
+            computerScoretext.textContent = 'Computer Score: '+computerscore.toString()
+            return
+        }
+        computerScoretext.textContent = 'Computer Score: '+computerscore.toString()
+        resultText.textContent = 'Result: You lose'
+    }
+    if(result=='Draw'){
+         resultText.textContent = 'Result: Its a Draw'
+    }
+}
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
@@ -12,34 +67,34 @@ function playRound(playerSelection,computerSelection){
 
     playerSelection = playerSelection.toLowerCase()
     if (playerSelection == computerSelection){
-        return 'Its a Draw!'
+        return 'Draw'
     }
     if(playerSelection=='rock'){
         if(computerSelection == 'scissors'){
-            return 'you Win! Rock beats scissors'
+            return 'Win'
         }
         else{
-            return 'you Lose! paper beats rock'
+            return 'Lose'
         }
     }
     if(playerSelection=='paper'){
         if(computerSelection == 'rock'){
-            return 'you Win! paper beats rock'
+            return 'Win'
         }
         else{
-            return 'you Lose! scissors beats rock'
+            return 'Lose'
         }
     }
     if(playerSelection=='scissors'){
         if(computerSelection == 'paper'){
-            return 'you Win! scissors beats paper'
+            return 'Win'
         }
         else{
-            return 'you Lose! rock beats scissors'
+            return 'Lose'
         }
     }
 }
-
+/*
 function game(){
     for(let i=1;i<=5;i++){
         playerSelection = prompt('make your choice:')
@@ -53,9 +108,13 @@ function game(){
         
     }
 }
+*/
 
-const moves = ["rock", 'paper', 'scissors']
-game()
+
+
+
+
+
 
 /*
 const playerSelection = "rock";
